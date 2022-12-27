74d3e401401e319655f1707c5ed27c2c67706154
Franco Arriola <francosebastianarriola@gmail.com>
2022-12-27 20:52:34 -0300

    acomodando css 7

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import Styles from "./index.module.scss";
 
 const Home = () => {
-  return <div className={Styles["nav"]}></div>;
+  return (
+    <div className={Styles["nav"]}>
+      <h1 className={Styles["title"]}>JULI Y FRANCO</h1>
+    </div>
+  );
 };
 
 export default Home;
