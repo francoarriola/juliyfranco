25c0078d9e25e377b2826ba11a7a2d70ab831291
Franco Arriola <francosebastianarriola@gmail.com>
2023-01-04 13:31:30 -0300

    strong en descripcion

--- src/Components/Where/index.js
+++ src/Components/Where/index.js
@@ -10,11 +10,11 @@ const Where = () => {
       <div className={Styles["desc"]}>
         <h1>Querida Familia y Amigos</h1>
         <p>
-          Nos casamos y nos despedimos de toda la gente que elegimos llevar en
-          nuestros corazones. <br />
+          Nos <strong>casamos y nos despedimos</strong> de toda la gente que
+          elegimos llevar en nuestros corazones. <br />
           Esta invitacion es especial porque festejar junto a ustedes seria un
           gran regalo.
-          <br /> La fiesta sera el 18 de febrero.
+          <br /> La fiesta sera el <strong> 18 de febrero.</strong>
           <br /> Se busca pastor aunque sea Aleman :).
           <br /> Codigo de vestimenta: Como te sientas comodo y L-Gante :)
           <br />
